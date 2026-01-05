15bdda9ba8810bc5f5900cbc0648a1457a374196
Youan Justin TRAH <justintrah8@gmail.com>
2026-01-05 10:37:20 +0000

    charts added

--- assets/js/pages/datatables.init.js
+++ assets/js/pages/datatables.init.js
@@ -1,9 +1,44 @@
 $(document).ready(function () {
-  $("#datatable").DataTable(),
+  $("#datatable").DataTable({
+    language: {
+      paginate: {
+        previous: "Précédent",
+        next: "Suivant",
+        first: "Premier",
+        last: "Dernier",
+      },
+      lengthMenu: "Affiche _MENU_ éléments",
+      search: "Recherche",
+      info: "Affichage de _START_ à _END_ sur _TOTAL_ éléments",
+      infoEmpty: "Affichage de 0 à 0 sur 0 éléments",
+      infoFiltered: "(filtré à partir de _MAX_ éléments au total)",
+      emptyTable: "Aucune donnée disponible dans le tableau",
+      zeroRecords: "Aucun enregistrement correspondant trouvé",
+      loadingRecords: "Chargement...",
+      processing: "Traitement...",
+    },
+  }),
     $("#datatable-buttons")
       .DataTable({
         lengthChange: !1,
         buttons: ["copy", "excel", "pdf", "colvis"],
+        language: {
+          paginate: {
+            previous: "Précédent",
+            next: "Suivant",
+            first: "Premier",
+            last: "Dernier",
+          },
+          lengthMenu: "Affiche _MENU_ éléments",
+          search: "Recherche",
+          info: "Affichage de _START_ à _END_ sur _TOTAL_ éléments",
+          infoEmpty: "Affichage de 0 à 0 sur 0 éléments",
+          infoFiltered: "(filtré à partir de _MAX_ éléments au total)",
+          emptyTable: "Aucune donnée disponible dans le tableau",
+          zeroRecords: "Aucun enregistrement correspondant trouvé",
+          loadingRecords: "Chargement...",
+          processing: "Traitement...",
+        },
       })
       .buttons()
       .container()
